8a0313dd5a4588bb9cb59849d492bb04cb639b0d
Alex Portnoy <Mehaletz@gmail.com>
2021-05-02 14:50:11 +0300

    Added in SplashActivity method enableViewLogo

--- app/src/main/java/com/example/topsportapp/splash/SplashActivity.java
+++ app/src/main/java/com/example/topsportapp/splash/SplashActivity.java
@@ -3,14 +3,34 @@ package com.example.topsportapp.splash;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.example.topsportapp.R;
 
 public class SplashActivity extends AppCompatActivity {
 
+    ImageView logoImage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        init();
+        enabledViewForLogo();
+    }
+
+    public void init() {
+        logoImage = findViewById(R.id.image_view_logo_top_sport);
+        logoImage.setAlpha(0f);
+        logoImage.setScaleX(0.5f);
+        logoImage.setScaleY(0.5f);
+        logoImage.setRotationX(90);
+
+    }
+
+    public void enabledViewForLogo() {
+        logoImage.animate().alpha(1).setDuration(4000);
+        logoImage.animate().scaleX(1.2f).scaleY(1.2f).setDuration(3000);
+        logoImage.animate().rotationX(0).setDuration(4000);
     }
 }
